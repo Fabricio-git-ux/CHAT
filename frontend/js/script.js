19c584e9d05d7e04cdc989fe36239593eef0fa66
Fabricio <fc746828628@gmail.com>
2025-10-27 04:45:31 -0300

    caminho

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -95,7 +95,7 @@ const handleLogin = (event) => {
     login.style.display = 'none';
     chat.style.display = 'flex';
 
-    websocket = new WebSocket('ws://localhost:8080');
+    websocket = new WebSocket("wss://conversation-chat-9e2x.onrender.com");
     websocket.onmessage = processMessage;
 }
 
